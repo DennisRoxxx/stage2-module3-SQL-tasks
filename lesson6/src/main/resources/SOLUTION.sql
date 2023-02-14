b49ed5888edf183083fe7a84c8d6c0ef87210e30
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2023-02-14 16:18:35 +0300

    Update SOLUTION.sql

--- lesson6/src/main/resources/SOLUTION.sql
+++ lesson6/src/main/resources/SOLUTION.sql
@@ -1,4 +1,4 @@
 SELECT * FROM payment WHERE type_id = (SELECT id FROM paymenttype WHERE name = 'MONTHLY');
 SELECT * FROM mark WHERE subject_id = (SELECT id FROM subject WHERE name = 'Art');
-SELECT s.* FROM student s JOIN payment p ON p.student_id = s.id JOIN paymenttype pt ON pt.id = p.type_id WHERE pt.name='WEEKLY';
+SELECT Student.* FROM Student INNER JOIN payment on payment.student_id = student.id WHERE payment.type_id = (SELECT id FROM PaymentType WHERE name='WEEKLY') GROUP BY student.id;
 SELECT s.* FROM mark m JOIN subject s ON m.subject_id = s.id JOIN student st ON m.student_id = st.id WHERE s.name = 'Math';
