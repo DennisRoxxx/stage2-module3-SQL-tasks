7dd6b648938c2212d1f30d5b33fc0956101dc6c6
Denis <106114126+DennisRoxxx@users.noreply.github.com>
2023-02-14 16:32:19 +0300

    Update SOLUTION.sql

--- lesson9/src/main/resources/SOLUTION.sql
+++ lesson9/src/main/resources/SOLUTION.sql
@@ -1,3 +1,3 @@
 SELECT * FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING AVG (mark) > 8);
 SELECT id, name FROM student WHERE id IN (SELECT student_id FROM mark GROUP BY student_id HAVING MIN (mark) > 7);
-SELECT id, name FROM student WHERE id IN (SELECT student_id FROM payment pt WHERE payment_date >= '2019-01-01' GROUP BY student_id HAVING count (*) > 2);
+SELECT id, name FROM student WHERE id IN (SELECT student_id FROM payment pt WHERE payment_date between '2019-01-01' and '2019-12-31' GROUP BY student_id HAVING count (*) > 2);
